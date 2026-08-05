df94fe122fd0a1cfe2839cfd45d72cd7f4665ab1
Pietro-MadeTech <pietro.convalle@justice.gov.uk>
2026-08-05 09:46:22 +0100

    Adds GL healthcheck and dependencies (#2)
    * Adds GL healthcheck and dependencies
    
    * Updated healthcheck test

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinanceholdsapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinanceholdsapi/config/WebClientConfiguration.kt
@@ -3,7 +3,9 @@ package uk.gov.justice.digital.hmpps.prisonerfinanceholdsapi.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.hmpps.kotlin.auth.authorisedWebClient
 import uk.gov.justice.hmpps.kotlin.auth.healthWebClient
 import java.time.Duration
 
@@ -12,8 +14,23 @@ class WebClientConfiguration(
   @param:Value("\${hmpps-auth.url}") val hmppsAuthBaseUri: String,
   @param:Value("\${api.health-timeout:2s}") val healthTimeout: Duration,
   @param:Value("\${api.timeout:20s}") val timeout: Duration,
+  @Value("\${general-ledger-api.url}") private val generalLedgerApiBaseUri: String,
 ) {
   // HMPPS Auth health ping is required if your service calls HMPPS Auth to get a token to call other services
   @Bean
   fun hmppsAuthHealthWebClient(builder: WebClient.Builder): WebClient = builder.healthWebClient(hmppsAuthBaseUri, healthTimeout)
+
+  @Bean
+  fun generalLedgerHealthWebClient(builder: WebClient.Builder): WebClient = builder.healthWebClient(generalLedgerApiBaseUri, healthTimeout)
+
+  @Bean
+  fun generalLedgerApiWebClient(
+    authorizedClientManager: OAuth2AuthorizedClientManager,
+    builder: WebClient.Builder,
+  ): WebClient = builder.authorisedWebClient(
+    authorizedClientManager = authorizedClientManager,
+    registrationId = "general-ledger-api",
+    url = generalLedgerApiBaseUri,
+    timeout = timeout,
+  )
 }
